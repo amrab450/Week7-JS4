260f8227eb85c875f28535065ab6617504c15ad3
amrab450 <abmrabouli@gmail.com>
2024-07-20 09:39:15 -0700

    index&scripts

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 
 
-//1. Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
+//1. Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93...
 let ages = [3, 9, 23, 64, 2, 8, 28, 93];
 
 // a.Programmatically subtract the value of the first element in the array from the value in the last element of the array.
